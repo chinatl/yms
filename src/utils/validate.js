0c8bc4585495f1e546fc714bdfc92092c4058a13
KellaTe <KellaTe@163.com>
2018-12-12 09:06:17 +0800

    提交版本。

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -6,3 +6,10 @@ export function validateIdCard(value) {
     const reg = /^(^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$)|(^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])((\d{4})|\d{3}[Xx])$)$/;
     return reg.test(value)
 }
+export function validateMob(value) {
+    // const reg = /^([0-9]{3,4}-)?[0-9]{7,8}$/
+    // const reg = /^(([0\+]\d{2,3}-)?(0\d{2,3})-)(\d{7,8})(-(\d{3,}))?$/
+    // const reg = /((d{3,4})|d{3,4}-)?d{7,8}(-d{3})*/
+    const reg = /^\d{3}-\d{7,8}|\d{4}-\d{7,8}$/
+    return reg.test(value)
+}
